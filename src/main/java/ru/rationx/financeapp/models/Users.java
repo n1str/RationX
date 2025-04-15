67f4355a7d3e7bdd2b5a064467453af93401d5e5
matthewencore <kirillkalmykov88@gmail.com>
2025-04-15 20:53:57 +0300

    добавил модель users

---
+++ src/main/java/ru/rationx/financeapp/models/Users.java
@@ -0,0 +1,16 @@
+package ru.rationx.financeapp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
