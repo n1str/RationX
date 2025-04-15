69134acbc62ca2778aa31b75204b04dd9cd17f24
n1str <hardminesu@gmail.com>
2025-04-15 21:10:18 +0300

    v.1.1

---
+++ src/main/java/ru/rationx/financeapp/models/Transaction.java
@@ -0,0 +1,67 @@
+package ru.rationx.financeapp.models;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class Transaction {
+
+    public enum PersonType {
+        ФИЗИЧЕСКОЕ_ЛИЦО, ЮРИДИЧЕСКОЕ_ЛИЦО
+    }
+
+    public enum TransactionType {
+        ПОСТУПЛЕНИЕ, СПИСАНИЕ
+    }
+
+    public enum TransactionStatus {
+        НОВАЯ,
+        ПОДТВЕРЖДЕННАЯ,
+        В_ОБРАБОТКЕ,
+        ОТМЕНЕНА,
+        ПЛАТЕЖ_ВЫПОЛНЕН,
+        ПЛАТЕЖ_УДАЛЕН,
+        ВОЗВРАТ
+    }
+
+    @NotNull
+    private PersonType personType;
+
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @NotNull
+    private TransactionType transactionType;
+
+    private String comment;
+
+    @DecimalMin(value = "0.01", inclusive = true)
+    @DecimalMax(value = "999999.99", inclusive = true)
+    @Digits(integer = 6, fraction = 2)
+    @NotNull
+    private Double amount;
+
+    @NotNull
+    private TransactionStatus status;
+
+    @NotBlank
+    private String senderBank;
+
+    @NotBlank
+    private String accountNumber;
+
+    @NotBlank
+    private String recipientBank;
+
+    @Pattern(regexp = "\\d{11}")
+    private String recipientInn;
+
+    private String recipientAccount;
+
+    @NotBlank
+    private String category;
+
+    @Pattern(regexp = "^(\\+7|8)\\d{10}$")
+    private String recipientPhone;
+}
